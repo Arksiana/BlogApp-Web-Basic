d28e350a463bd6d842fe262541917f7672e73efb
Arksiana <74001470+Arksiana@users.noreply.github.com>
2023-02-27 18:42:01 +0200

    update

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from website import views
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
@@ -11,10 +11,23 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    return render_template('login.html')
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect!', category='error')
+    else:
+        flash('Email does not exist!', category='error')
+
+    return render_template('login.html', user=current_user)
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -27,7 +40,7 @@ def sign_up():
 
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
-        
+
         if email_exists:
             flash('Email already exists.', category='error')
         elif username_exists:
@@ -41,15 +54,19 @@ def sign_up():
         elif len(email) < 4:
             flash('Email is invalid.', category='error')
         else:
-            new_user = User(email=email, username=username, password=password1)
+            new_user = User(email=email, username=username, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created!', category='success')
-            return redirect(url_for(views.home))
+            return redirect(url_for('views.home'))
 
-    return render_template('signup.html')
+    return render_template('signup.html', user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return redirect(url_for('views.html'))
+    logout_user()
+    return redirect(url_for('views.login'))
